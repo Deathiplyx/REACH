037afc57d7f6b67096db72e59f758ec73b294b09
James Carnathan <Jamescarnathan2006@gmail.com>
2026-02-18 23:56:38 -0500

    edits from MYSELF

--- processes/ask.py
+++ processes/ask.py
@@ -1,1 +1,2 @@
-# this script is used to open chatgpt on google chrome with a specific query and then hit enter, using our premade code in the atoms folder
+# this script is used to open chatgpt on google chrome with a specific query and then hit enter
+#  using our premade code in the atoms folder
